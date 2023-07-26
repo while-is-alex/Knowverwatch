c238245b92f0cb6f5cfdcf21a3179d4c2d0e21ca
Alexandre Rodrigues <arodriguesclasses@gmail.com>
2023-07-26 15:29:50 -0300

    feat(views/urls): add logic and paths for teams page, team details page and players page

--- stats/urls.py
+++ stats/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+from stats import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.HomeView.as_view(), name='home-page'),
+    path('teams', views.TeamsView.as_view(), name='teams-page'),
+    path('players', views.PlayersView.as_view(), name='players-page'),
+    path('teams/<team_id>', views.TeamDetailsView.as_view(), name='team-details-page'),
+    # path('players/<player>'),
 ]
--- stats/views.py
+++ stats/views.py
@@ -1,9 +1,61 @@
 from OWLAPI import Owl
 from django.shortcuts import render
+from django.views import View
 
 
-def index(request):
-    return render(
-        request,
-        'stats/index.html',
-    )
+class HomeView(View):
+    def get(self, request):
+        return render(
+            request,
+            'stats/index.html',
+            {
+
+            }
+        )
+
+
+class TeamsView(View):
+    def get(self, request):
+        owl = Owl()
+        all_teams = owl.all_teams()
+
+        return render(
+            request,
+            'stats/teams.html',
+            {
+                'teams': all_teams,
+            }
+        )
+
+
+class TeamDetailsView(View):
+    def get(self, request, team_id):
+        owl = Owl()
+        selected_team = owl.get_team(team_id)
+        roster = []
+        for player in selected_team['roster']:
+            current_player = owl.get_player(player)
+            roster.append(current_player)
+
+        return render(
+            request,
+            'stats/team-details.html',
+            {
+                'team': selected_team,
+                'players': roster,
+            }
+        )
+
+
+class PlayersView(View):
+    def get(self, request):
+        owl = Owl()
+        all_players = owl.all_players()
+
+        return render(
+            request,
+            'stats/players.html',
+            {
+                'players': all_players,
+            }
+        )

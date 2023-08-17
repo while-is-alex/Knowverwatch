1e643441a42456c194ed7bfef9d731ff861c4dcb
Alexandre Rodrigues <arodriguesclasses@gmail.com>
2023-08-17 20:26:37 -0300

    feat(classes): add logic to order statistics and to sort them by time played
    Add new method sort_by_time_played to the Stats class to sort the heroes by time played. Add new logic to the format_details method to establish an order to the statistics received from the API.

--- classes.py
+++ classes.py
@@ -1,3 +1,6 @@
+import collections
+
+
 class Stats:
     def split_camel_case(self, camel_case_string):
         words = [[camel_case_string[0]]]
@@ -13,10 +16,29 @@ class Stats:
 
         return resulting_string
 
+    def sort_by_time_played(self, stats_dictionary):
+        def get_time_played(current_hero):
+            try:
+                time_played = current_hero[1]['timePlayed']
+                return time_played
+            except KeyError:
+                time_played = 0
+                return time_played
+
+        sorted_heroes_by_time_played = dict(collections.OrderedDict(
+            sorted(
+                stats_dictionary.items(),
+                key=get_time_played,
+                reverse=True
+            )
+        ))
+
+        return sorted_heroes_by_time_played
+
     def format_details(self, hero_name, stats_dictionary):
         stats_dictionary_keys = stats_dictionary.keys()
 
-        hero_stats = []
+        stats_list = [None] * 12
         for key in stats_dictionary_keys:
             stat_name = self.split_camel_case(key)
             if stat_name == 'Hero Damage Done':
@@ -26,10 +48,40 @@ class Stats:
 
             current_stat = {'name': stat_name, 'value': stat_value}
 
-            hero_stats.append(current_stat)
+            if current_stat['name'] == 'Time Played':
+                stats_list[0] = current_stat
+            elif current_stat['name'] == 'Eliminations':
+                stats_list[1] = current_stat
+            elif current_stat['name'] == 'Final Blows':
+                stats_list[2] = current_stat
+            elif current_stat['name'] == 'Deaths':
+                stats_list[3] = current_stat
+            elif current_stat['name'] == 'Damage Done':
+                stats_list[4] = current_stat
+            elif current_stat['name'] == 'Healing Done':
+                stats_list[5] = current_stat
+            elif current_stat['name'] == 'Damage Taken':
+                stats_list[6] = current_stat
+            elif current_stat['name'] == 'Shots Hit':
+                stats_list[7] = current_stat
+            elif current_stat['name'] == 'Critical Hits':
+                stats_list[8] = current_stat
+            elif current_stat['name'] == 'Ults Used':
+                stats_list[9] = current_stat
+            elif current_stat['name'] == 'Time Spent On Fire':
+                stats_list[10] = current_stat
+            elif current_stat['name'] == 'Solo Kills':
+                stats_list[11] = current_stat
+            elif current_stat['name'] == 'Ults Earned':
+                continue
+            else:
+                stats_list.append(current_stat)
 
-        for stat in hero_stats:
-            if stat['name'] == 'Time Played' and stat['value'] > 60:
-                hero_details = (hero_name.title(), hero_stats)
+        for stat in stats_list:
+            try:
+                if stat['name'] == 'Time Played' and stat['value'] > 60:
+                    hero_details = (hero_name.title(), stats_list)
 
-                return hero_details
+                    return hero_details
+            except TypeError:
+                continue

f179d7c31c459ffe55a81ad130162c33c975caa5
Alexandre Rodrigues <arodriguesclasses@gmail.com>
2023-08-17 18:03:42 -0300

    refactor(classes): refactor code to make it more readable

--- classes.py
+++ classes.py
@@ -1,11 +1,8 @@
-import re
-
-
 class Stats:
-    def split_camel_case(self, str):
-        words = [[str[0]]]
+    def split_camel_case(self, camel_case_string):
+        words = [[camel_case_string[0]]]
 
-        for c in str[1:]:
+        for c in camel_case_string[1:]:
             if words[-1][-1].islower() and c.isupper():
                 words.append(list(c))
             else:
@@ -13,15 +10,26 @@ class Stats:
 
         words = [''.join(word) for word in words]
         resulting_string = ' '.join(words).title()
+
         return resulting_string
 
     def format_details(self, hero_name, stats_dictionary):
-        keys = stats_dictionary.keys()
-        hero_stats = []
-        for key in keys:
-            statistic = {'name': self.split_camel_case(key), 'value': int(stats_dictionary[key])}
-            hero_stats.append(statistic)
+        stats_dictionary_keys = stats_dictionary.keys()
 
-        if len(hero_stats) > 3:
-            hero_details = (hero_name.title(), hero_stats)
-            return hero_details
+        hero_stats = []
+        for key in stats_dictionary_keys:
+            stat_name = self.split_camel_case(key)
+            if stat_name == 'Hero Damage Done':
+                stat_name = 'Damage Done'
+
+            stat_value = int(stats_dictionary[key])
+
+            current_stat = {'name': stat_name, 'value': stat_value}
+
+            hero_stats.append(current_stat)
+
+        for stat in hero_stats:
+            if stat['name'] == 'Time Played' and stat['value'] > 60:
+                hero_details = (hero_name.title(), hero_stats)
+
+                return hero_details

9eb39d8b3611e9b9a2d954aa4bc62afb000d5f71
Alexandre Rodrigues <arodriguesclasses@gmail.com>
2023-09-13 23:12:24 -0300

    refactor(directories): move the OWLAPI file to the utilities directory

--- utilities/OWLAPI.py
+++ utilities/OWLAPI.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 
 
-class Owl:
+class OverwatchLeague:
     def __init__(self):
         """Generates an authorization token for the current session and
         initializes the endpoint and params for the http request."""
